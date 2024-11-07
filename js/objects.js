023c0230bcce1c806a4e42eb888ebd46314c22ca
Filip Juran <filipjuranoe@gmail.com>
2024-11-07 01:10:06 +0100

    JS - Object Excercise

---
+++ js/objects.js
@@ -0,0 +1,12 @@
+function Book(title, author, year, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.pages = pages;
+    this.read = read;
+    this.sayInfo = function () {
+        return this
+    }
+}
+const AnimalFarm = new Book("AnimalFarm", "G. Orwell", 1945, 176, true)
+console.log(AnimalFarm.sayInfo())
